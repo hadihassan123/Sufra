6595eaccd2e23c04e181599d18c62ab554a8276f
hadihassan123 <hadihassan.aa123@gmail.com>
2026-07-27 12:46:46 +0300

    map rebuild 27th 11th

--- js/customer.js
+++ js/customer.js
@@ -29,7 +29,7 @@
     if (!map) {
       console.log("Initializing new Leaflet map...");
       map = L.map('mapView').setView([25.30, 51.51], 12);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap'
       }).addTo(map);
     }
